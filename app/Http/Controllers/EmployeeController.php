673cdcd92e5d807bb3d76eb1b0fa74926192fce4
Tran Duong <tranduong6114@gmail.com>
2026-06-26 10:28:01 +0700

    feat: calculate and export social insurance to CSV

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,21 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Services\EmployeeService;
+use App\Services\InsuranceService;
 use Illuminate\Http\JsonResponse;
 
 class EmployeeController extends Controller
 {
     protected EmployeeService $employeeService;
+    protected InsuranceService $insuranceService;
 
-    public function __construct(EmployeeService $employeeService)
+    public function __construct(EmployeeService $employeeService, InsuranceService $insuranceService)
     {
         $this->employeeService = $employeeService;
+        $this->insuranceService = $insuranceService;
     }
 
     public function importCsv(): JsonResponse
     {
         $filePath = storage_path('app/import/nhanvien.csv');
         $result = $this->employeeService->importFromPath($filePath);
+        return response()->json($result, $result['success'] ? 200 : 400, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    public function exportBhxh(): JsonResponse
+    {
+        $result = $this->insuranceService->calculateAndExport();
         return response()->json($result, $result['success'] ? 200 : 400, [], JSON_UNESCAPED_UNICODE);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/import-csv', [EmployeeController::class, 'importCsv'])
     ->name('employees.importCsv');
+
+Route::get('/export-bhxh', [EmployeeController::class, 'exportBhxh'])
+    ->name('employees.exportBhxh');
